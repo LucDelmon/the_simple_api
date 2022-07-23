98e31f031af31c5dc233cee75fc069ac0ba33fc0
LucDelmon <luc.delmon@gmail.com>
2022-07-23 06:15:09 +0200

    feat: add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter 'app/channels/application_cable/channel.rb'
+  add_filter 'app/channels/application_cable/connection.rb'
+  add_filter 'app/jobs/application_job.rb'
+  add_filter 'app/mailers/application_mailer.rb'
+end
 require 'database_cleaner/active_record'
 
 RSpec.configure do |config|
