fbd74cf1c1d99f9bb52b837096e26391019c3223
LucDelmon <luc.delmon@gmail.com>
2022-08-12 18:17:51 +0200

    feat:  add sidekiq to the project

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,12 @@ module TheSimpleApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.api_only = true
+
+    config.session_store :cookie_store, key: '_interslice_session'
+
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+
+    config.middleware.use config.session_store, config.session_options
   end
 end
